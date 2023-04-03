8b4616cbee335a63401e1f8ac216fe70ecdcf8b7
Rahul Sharma <rahul32412@gmail.com>
2023-04-03 19:52:53 +0530

    contact added

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import { useState } from "react";
+import "../styles/Contact.css";
 
 function Contact() {
-  return <div>Contact</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Your form has been submitted");
+  };
+  return (
+    <div className="contact">
+      <form onSubmit={handleSubmit}>
+        <div className="form-item">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+            required
+          />
+        </div>
+        <div className="form-item">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+            required
+          />
+        </div>
+        <div className="form-item">
+          <label htmlFor="message">Message</label>
+          <textarea
+            type="text"
+            id="message"
+            value={message}
+            onChange={(event) => {
+              setMessage(event.target.value);
+            }}
+          />
+        </div>
+        <button type="submit" className="btn-submit">
+          Send
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Contact;
